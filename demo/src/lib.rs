704cf5c663af9a9c1fcfcfd0d3d71f6e5a21776a
Jonas Fiala <jonas.fiala.cardiff@gmail.com>
2023-03-09 09:21:58 +0000

    Update example function to avoid warning

--- demo/src/lib.rs
+++ demo/src/lib.rs
@@ -1,5 +1,6 @@
 use russol_contracts::*;
 
+// Example pure function
 #[pure]
 fn is_ok<T, V>(x: &Result<T, V>) -> bool {
     matches!(x, Ok(_))
@@ -12,13 +13,21 @@ fn foo<T, V>(x: &mut Result<T, V>) -> (bool, Result<&mut V, &mut T>) {
     todo!()
 }
 
+// This function will not be synthesized (no #[synth] annotation)
 #[ensures(result == x as i32 + y as i32)]
 fn bar(x: i8, y: i8) -> i32 {
     todo!()
 }
 
+#[pure]
+fn is_none<T>(x: &Option<T>) -> bool {
+    matches!(x, None)
+}
+
+// Example function which can be used (called)
+// by synthesized implementations
 #[extern_spec]
-#[ensures(matches!(^x, None))]
+#[ensures(is_none(&^x))]
 #[ensures(*x === result)]
 fn take<T>(x: &mut Option<T>) -> Option<T> {
     std::mem::take(x)
